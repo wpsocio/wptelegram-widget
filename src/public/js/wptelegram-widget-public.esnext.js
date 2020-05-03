416ed0e87d49764686a2f3a792d1922db4abdde9
Manzoor Wani <manzoorwani.jk@gmail.com>
2020-05-04 00:43:09 +0530

    Updatte public JS for widget height

--- src/public/js/wptelegram-widget-public.esnext.js
+++ src/public/js/wptelegram-widget-public.esnext.js
@@ -1,45 +1,35 @@
-( function( $ ) {
+(function ($) {
 	'use strict';
 	const widget = {};
 	widget.configure = () => {
-		widget.wrap = $( '.wptelegram-widget-wrap' );
-		widget.iframe = $( '.wptelegram-widget-message iframe' );
+		widget.wrap = $('.wptelegram-widget-wrap');
+		widget.iframe = $('.wptelegram-widget-message iframe');
 	};
 	widget.init = () => {
 		widget.configure();
-		widget.iframe.on( 'load', widget.set_iframe );
+		widget.iframe.on('load', widget.set_iframe);
 		// widget.iframe.on( 'load', widget.resize_iframe );
-		widget.iframe.on( 'resize_iframe', widget.resize_iframe );
+		widget.iframe.on('resize_iframe', widget.resize_iframe);
 
-		$( window ).on( 'resize', () => {
-			widget.iframe.trigger( 'resize_iframe' );
-		} );
+		$(window).on('resize', () => {
+			widget.iframe.trigger('resize_iframe');
+		});
 	};
-	widget.set_iframe = () => {
-		const $this = $( this );
-		if (
-			$this
-				.contents()
-				.find( 'body' )
-				.is( ':empty' )
-		) {
+	widget.set_iframe = function () {
+		const $this = $(this);
+		if ($this.contents().find('body').is(':empty')) {
 			$this.parent().remove();
 			// reconfigure
 			widget.configure();
 		} else {
-			$this.trigger( 'resize_iframe' );
+			$this.trigger('resize_iframe');
 		}
 	};
-	widget.resize_iframe = () => {
+	widget.resize_iframe = function () {
 		// this.style.height = this.contentWindow.document.body.scrollHeight + 'px';
-		$( this ).height(
-			$( this )
-				.contents()
-				.find( 'body' )
-				.height()
-		);
+		$(this).height($(this).contents().find('body').height());
 	};
 
 	// trigger on $(document).ready();
-	$( widget.init );
-} )( jQuery );
+	$(widget.init);
+})(jQuery);
