93f2e7742eb2f8f8d556be1216afb9dc52a0cdf1
GitHub Action <github-action@users.noreply.github.com>
2021-04-21 08:17:55 +0000

    Deployed from manzoorwanijk/wp-plugins@34e246dde4414be4f1fe7a1839a08a66fd163167

---
+++ src/assets/static/js/wptelegram-widget--blocks.438eb10a.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-url'), 'version' => '21e45248b998021158312589dc6c5c79');
